7f33bd480846baecb1c8be5b03545e7f710f9fbc
vinh <nguyenhoaivinh.1111@gmail.com>
2019-09-11 10:37:27 +0700

    about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,36 +1,21 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Row from "react-bootstrap/Row"
 import styles from "../components/about-css-modules.module.css"
-import Col from "react-bootstrap/Col"
 
 const AboutPage = () => {
   return (
     <Layout>
-      <div style={{ marginLeft: `20px` }}>
-        <SEO title="About us"></SEO>
-        <Row>
-          <Col>
-            <h3>About us</h3>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <p>
-              We're creating an environment for every one to enjoy the
-              atmosphere.
-            </p>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Link to="/">Home page</Link>
-          </Col>
-        </Row>
-        <Row>
+      <div className={styles.Container}>
+        <SEO title="About us" />
+        <div className={styles.AboutContent}>
+          <h3>About us</h3>
+          <p>
+            We're creating an environment for every one to enjoy the atmosphere.
+          </p>
+        </div>
+        <div className={styles.map}>
           <iframe
             title="our place"
             className={styles.iframe}
@@ -40,7 +25,7 @@ const AboutPage = () => {
             }
             allowFullScreen
           ></iframe>
-        </Row>
+        </div>
       </div>
     </Layout>
   )

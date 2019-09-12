ab7a8a1b521cb938aded1c13626a05b7f8b82d05
vinh <nguyenhoaivinh.1111@gmail.com>
2019-09-12 10:40:17 +0700

    edit email form submission

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -17,11 +17,34 @@ const ContactPage = () => (
 					</FacebookProvider>
 				</div>
 				<div>
-					<form action="https://formspree.io/email@domain.tld" method="POST">
-						<input type="text" name="name" />
-						<input type="email" name="_replyto" />
-						<input type="submit" value="Send" />
-						<input type="hidden" name="_next" value="http://localhost:8000/about" />
+					<form name="contact" method="POST" data-netlify="true">
+						<p>
+							<label>
+								Your Name: <input type="text" name="name" />
+							</label>
+						</p>
+						<p>
+							<label>
+								Your Email: <input type="email" name="email" />
+							</label>
+						</p>
+						<p>
+							<label>
+								Your Role:{' '}
+								<select name="role[]" multiple>
+									<option value="leader">Leader</option>
+									<option value="follower">Follower</option>
+								</select>
+							</label>
+						</p>
+						<p>
+							<label>
+								Message: <textarea name="message" />
+							</label>
+						</p>
+						<p>
+							<button type="submit">Send</button>
+						</p>
 					</form>
 				</div>
 			</div>

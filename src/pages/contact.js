bafaedacc369940989857ef4184619b1218fda9e
vinh <nguyenhoaivinh.1111@gmail.com>
2019-09-12 17:31:10 +0700

    add zoom image and re style pages

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,23 +10,55 @@ const ContactPage = () => (
 		<div className={styles.ContactContainer}>
 			<SEO title="Contact" />
 			<div className={styles.ContactContent}>
-				<h1>Contact</h1>
-				<div style={{ height: 'auto', marginBottom: '10px' }}>
-					<FacebookProvider appId={`${process.env.APP_ID}`}>
-						<Page href="https://www.facebook.com/StreetBeerMD" tabs="timeline" />
-					</FacebookProvider>
-				</div>
-				<div>
+				<h1>Visit our fan page</h1>
+				{/* <FacebookProvider appId={`${process.env.APP_ID}`}>
+					<Page href="https://www.facebook.com/StreetBeerMD" tabs="timeline" />
+				</FacebookProvider> */}
+				<div id="fb-root" />
+				<iframe
+					src={
+						'https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FStreetBeerMD&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=392286994820189'
+					}
+					width="340"
+					height="500"
+					style={{ border: 'none', overflow: 'hidden', margin: 'auto' }}
+					scrolling="no"
+					frameBorder="0"
+					allow="encrypted-media"
+				/>
+				<h2>Tell us what you think</h2>
+				<div className={styles.FeedbackForm}>
 					<form method="post" action="#" data-netlify="true" name="feedback">
 						<input type="hidden" name="form-name" value="feedback" />
-						<input type="text" name="name" id="name" required />
-						<input type="text" name="email" id="email" required />
-						<textarea name="message" id="message" rows="4" required />
-						<input type="submit" value="Send Message" />
+						<p>
+							<label>Your Name:</label>
+							<input type="text" name="name" id="name" required />
+						</p>
+						<p>
+							<label>Your Email:</label>
+							<input type="email" name="email" id="name" required />
+						</p>
+						<p>
+							<label>Message:</label>
+							<textarea name="message" id="message" rows="4" required />
+						</p>
+						<p>
+							<input id="button" type="submit" value="Submit Your Feedback" />
+						</p>
 					</form>
 				</div>
 			</div>
 		</div>
+		{/* <script
+			async
+			defer
+			crossorigin="anonymous"
+			src={
+				'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v4.0&appId=' +
+				`${process.env.APP_ID}` +
+				'&autoLogAppEvents=1'
+			}
+		/> */}
 	</Layout>
 );
 
--- src/pages/pages.js
+++ src/pages/pages.js
@@ -3,53 +3,79 @@ import React from 'react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 import styles from '../components/pages-css-modules.module.css';
+import Litebox from '../components/ZoomImage/Litebox';
 
 const Pages = () => (
 	<Layout>
 		<div className={styles.PagesContainer}>
 			<SEO title="Pages" />
 			<div className={styles.PagesContent}>
-				<h1>Pages</h1>
-				<img
-					className={styles.image}
-					src="https://amthuchathanh.com/wp-content/uploads/2010/11/muc-kho.jpg"
-					alt=""
-				/>
-				<img
-					className={styles.image}
-					src="https://dacsanngon3mien.net/wp-content/uploads/2017/11/muc-kho-da-nang-1.jpg"
-					alt=""
-				/>
-				<img
-					className={styles.image}
-					src="https://muckho.vn/wp-content/uploads/2015/07/muc-kho-tam-gia-vi-cay-cay.jpg"
-					alt=""
-				/>
-				<img
-					className={styles.image}
-					src="https://amthuchathanh.com/wp-content/uploads/2010/11/muc-kho.jpg"
-					alt=""
-				/>
-				<img
-					className={styles.image}
-					src="https://tse2.mm.bing.net/th?id=OIP.HkAXD6sq0ZJlREqExoSM9AHaE7&pid=Api"
-					alt=""
-				/>
-				<img
-					className={styles.image}
-					src="https://tse2.mm.bing.net/th?id=OIP.t7a5wgoUoy3imiuoRK8ruwHaFj&pid=Api"
-					alt=""
-				/>
-				<img
-					className={styles.image}
-					src="https://amthuchathanh.com/wp-content/uploads/2010/11/muc-kho.jpg"
-					alt=""
-				/>
-				<img
-					className={styles.image}
-					src="https://muckho.vn/wp-content/uploads/2015/07/muc-kho-tam-gia-vi-cay-cay.jpg"
-					alt=""
-				/>
+				<h1>Food</h1>
+				<p>We also sell dried squid</p>
+				<div className={styles.image}>
+					<Litebox
+						small="https://amthuchathanh.com/wp-content/uploads/2010/11/muc-kho.jpg"
+						large="https://amthuchathanh.com/wp-content/uploads/2010/11/muc-kho.jpg"
+					/>
+					<span>dried squid special</span>
+					<span>price: $10/Kg</span>
+				</div>
+				<div className={styles.image}>
+					<Litebox
+						small="https://dacsanngon3mien.net/wp-content/uploads/2017/11/muc-kho-da-nang-1.jpg"
+						large="https://dacsanngon3mien.net/wp-content/uploads/2017/11/muc-kho-da-nang-1.jpg"
+					/>
+					<span>dried squid</span>
+					<span>price: $10/Kg</span>
+				</div>
+				<div className={styles.image}>
+					<Litebox
+						small="https://img.21food.com/userimages/qingdaohanzhou/qingdaohanzhou$103110520.jpg"
+						large="https://img.21food.com/userimages/qingdaohanzhou/qingdaohanzhou$103110520.jpg"
+					/>
+					<span>dried squid</span>
+					<span>price: $10/Kg</span>
+				</div>
+				<div className={styles.image}>
+					<Litebox
+						small="https://amthuchathanh.com/wp-content/uploads/2010/11/muc-kho.jpg"
+						large="https://amthuchathanh.com/wp-content/uploads/2010/11/muc-kho.jpg"
+					/>
+					<span>dried squid special</span>
+					<span>price: $10/Kg</span>
+				</div>
+				<div className={styles.image}>
+					<Litebox
+						small="https://tse2.mm.bing.net/th?id=OIP.HkAXD6sq0ZJlREqExoSM9AHaE7&pid=Api"
+						large="https://tse2.mm.bing.net/th?id=OIP.HkAXD6sq0ZJlREqExoSM9AHaE7&pid=Api"
+					/>
+					<span>dried squid</span>
+					<span>price: $10/Kg</span>
+				</div>
+				<div className={styles.image}>
+					<Litebox
+						small="https://tse2.mm.bing.net/th?id=OIP.t7a5wgoUoy3imiuoRK8ruwHaFj&pid=Api"
+						large="https://tse2.mm.bing.net/th?id=OIP.t7a5wgoUoy3imiuoRK8ruwHaFj&pid=Api"
+					/>
+					<span>dried squid special</span>
+					<span>price: $10/Kg</span>
+				</div>
+				<div className={styles.image}>
+					<Litebox
+						small="https://amthuchathanh.com/wp-content/uploads/2010/11/muc-kho.jpg"
+						large="https://amthuchathanh.com/wp-content/uploads/2010/11/muc-kho.jpg"
+					/>
+					<span>dried squid special</span>
+					<span>price: $10/Kg</span>
+				</div>
+				<div className={styles.image}>
+					<Litebox
+						small="https://img.21food.com/userimages/qingdaohanzhou/qingdaohanzhou$103110520.jpg"
+						large="https://img.21food.com/userimages/qingdaohanzhou/qingdaohanzhou$103110520.jpg"
+					/>
+					<span>dried squid special</span>
+					<span>price: $10/Kg</span>
+				</div>
 			</div>
 		</div>
 	</Layout>

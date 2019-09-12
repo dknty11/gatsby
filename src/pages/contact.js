c6e9e4bfb2b641501a30dc745f32598dfbf8b0f1
vinh <nguyenhoaivinh.1111@gmail.com>
2019-09-12 10:09:19 +0700

    remove app id

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,27 +1,32 @@
-import React from "react"
-import { FacebookProvider, Page } from "react-facebook"
+import React from 'react';
+import { FacebookProvider, Page } from 'react-facebook';
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import styles from "../components/contact-css-modules.module.css"
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+import styles from '../components/contact-css-modules.module.css';
 
 const ContactPage = () => (
-  <Layout>
-    <div className={styles.ContactContainer}>
-      <SEO title="Contact" />
-      <div className={styles.ContactContent}>
-        <h1>Contact</h1>
-        <div style={{ height: "auto", marginBottom: "10px" }}>
-          <FacebookProvider appId="392286994820189">
-            <Page
-              href="https://www.facebook.com/StreetBeerMD"
-              tabs="timeline"
-            />
-          </FacebookProvider>
-        </div>
-      </div>
-    </div>
-  </Layout>
-)
+	<Layout>
+		<div className={styles.ContactContainer}>
+			<SEO title="Contact" />
+			<div className={styles.ContactContent}>
+				<h1>Contact</h1>
+				<div style={{ height: 'auto', marginBottom: '10px' }}>
+					<FacebookProvider appId={`${process.env.APP_ID}`}>
+						<Page href="https://www.facebook.com/StreetBeerMD" tabs="timeline" />
+					</FacebookProvider>
+				</div>
+				<div>
+					<form action="https://formspree.io/email@domain.tld" method="POST">
+						<input type="text" name="name" />
+						<input type="email" name="_replyto" />
+						<input type="submit" value="Send" />
+						<input type="hidden" name="_next" value="http://localhost:8000/about" />
+					</form>
+				</div>
+			</div>
+		</div>
+	</Layout>
+);
 
-export default ContactPage
+export default ContactPage;
